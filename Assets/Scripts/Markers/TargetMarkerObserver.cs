0140bd578557b02c839c5c4e8132a0e1ecf8dd84
Oleg Polishchuk <o.polishchuk@xlab-game.com>
2025-12-10 19:36:48 +0300

    HW2Commit

---
+++ Assets/Scripts/Markers/TargetMarkerObserver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Markers
+{
+	public class TargetMarkerObserver: MonoBehaviour
+	{
+		[SerializeField] private TargetMarker m_targetMarker;
+		[SerializeField] private PlayerMovement m_playerMovement;
+
+        private void OnEnable()
+        {
+            m_playerMovement.Stopped += OnPlayerStopped;
+            m_playerMovement.DestinationChanged += OnDestinationChanged;
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        private void OnPlayerStopped()
+        {
+            m_targetMarker.Hide();
+        }
+
+        private void OnDestinationChanged(Vector3 worldPosition)
+        {
+            m_targetMarker.Show(worldPosition);
+        }
+    }
+}
--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -1,13 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private TargetMarker m_targetMarker;
+    public event Action Stopped;
+    public event Action<Vector3> DestinationChanged;
+
     [SerializeField] private NavMeshAgent m_agent;
 
     private float m_speed;
+    private bool m_hasDestination;
 
     private void OnValidate()
     {
@@ -17,19 +21,41 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void SetDestination(Vector3 navMeshPoint)
-    {
-        m_targetMarker.Show(navMeshPoint);
-        m_agent.SetDestination(navMeshPoint);
-    }
-
     private void Awake()
     {
         Initialize(m_speed);
     }
+
+    private void Update()
+    {
+        if (!m_hasDestination || m_agent.pathPending)
+        {
+            return;
+        }
+
+        if (m_agent.remainingDistance <= m_agent.stoppingDistance)
+        {
+            if (!m_agent.hasPath || m_agent.velocity.sqrMagnitude <= 0.001f)
+            {
+                m_agent.isStopped = false;
+
+                Stopped?.Invoke();
+            }
+        }
+    }
+
     public void Initialize(float speed)
     {
         m_speed = speed;
         m_agent.speed = speed;
+    }
+
+    public void SetDestination(Vector3 navMeshPoint)
+    {
+        m_agent.SetDestination(navMeshPoint);
+
+        m_hasDestination = true;
+
+        DestinationChanged?.Invoke(navMeshPoint);
     }
 }
